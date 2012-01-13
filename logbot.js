aa421d21363267abde54c2d476c4d879abdd3362
Wil Asche <wasche@tripadvisor.com>
2012-01-13 17:23:16 -0500

    implement logging

--- logbot.js
+++ logbot.js
@@ -1,11 +1,91 @@
 #!/usr/bin/env node
 
 var irc = require('irc')
-  , client = new irc.Client('irc.tripadvisor.com', 'logbot', {
-      channels: ['#social']
+  , util = require('util')
+  , options = {
+      nick: 'logbot'
+    , server: 'irc.tripadvisor.com'
+    , channels: ['#social']
+    , log_url: 'http://irc.tripadvisor.com/irclogs/'
+    }
+  , client = new irc.Client(options.server, options.nick, {
+      channels: options.channels
+    , secure: false
     })
+  , fs = require('fs')
+  , daemon = require('daemon')
   ;
 
+function format(fmt, obj) {
+  return fmt.replace(/%s/g, function(match){ return obj.shift(); });
+}
+
+function strip(str) {
+  return str.replace(/^\s+|\s+$/, '');
+}
+
+function log(channel, message) {
+  var filename = './logs/%s-%s-%s-%s.txt'
+    , now = new Date()
+    , out
+    ;
+
+  filename = format(filename, [channel.substring(1), now.getFullYear(), now.getMonth()+1, now.getDate()]);
+
+  out = fs.createWriteStream(filename, {
+    flags     : 'a'
+  , encoding  : 'utf8'
+  , mode      : 0666
+  });
+  out.write(format('%s: %s\n', [now.toTimeString(), message]));
+}
+
+client.addListener('error', function(message) {
+  console.log(message);
+});
+
 client.addListener('message', function(from, to, message) {
-  console.log('[' + from + '] ' + to + ': ' + message);
+  var isChannel = ~(to.indexOf('#'))
+    , target = isChannel ? to : from
+    , response = false
+    ;
+
+  if (isChannel) {
+    log(target, format('<%s> %s', [from, message]));
+
+    if (~message.indexOf(options.nick + ':')) {
+      var content = strip(message.substring(options.nick.length + 2));
+      switch (content) {
+        case 'logs':
+          response = format('%s: %s', [from, options.log_url]);
+          break;
+      }
+      if (response) {
+        client.say(target, response);
+        log(target, format('<%s> %s', [options.nick, response]));
+      }
+    }
+  }
+});
+
+client.addListener('join', function(channel, who) {
+  log(channel, format('%s joined.', [who]));
+});
+
+client.addListener('part', function(channel, who, why) {
+  log(channel, format('%s left (%s).', [who, why]));
+});
+
+client.addListener('kick', function(channel, who, by, why) {
+  log(channel, format('%s was kicked by %s (%s).', [who, by, why]));
+});
+
+client.addListener('topic', function(channel, topic, who) {
+  log(channel, format('Topic is "%s" (set by %s)', [topic, who]));
+});
+
+
+daemon.daemonize('daemon.log', '/tmp/logbot.pid', function(err, pid) {
+  if (err) { return util.log('Error starting daemon: ' + err); }
+  util.log('Daemon started with pid: ' + pid);
 });
